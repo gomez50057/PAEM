3a611c48746b17ec8e3b0fc4bd2cdd84b8cba63a
gomez50057 <gomez.50057@gmail.com>
2024-08-14 14:35:07 -0600

    Slider

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -7,48 +7,48 @@ const slides = [
   {
     image: 'https://via.placeholder.com/250?text=Slide+1',
     title: 'Subcomisión de Seguridad Pública y Procuración de Justicia de la Zona Metropolitana de Tula',
-    description: 'Lograr mejores condiciones en materia de SEGURIDAD PÚBLICA Y PROCURACIÓN DE JUSTICIA, en beneficio de los habitantes de la Zona Metropolitana de Tula',
+    description: 'Lograr mejores condiciones en materia de SEGURIDAD PÚBLICA Y PROCURACIÓN DE JUSTICIA, en beneficio de los habitantes de la Zona Metropolitana de Tula.',
   },
   {
     image: 'https://via.placeholder.com/250?text=Slide+2',
     title: 'Subcomisión de Salud de la Zona Metropolitana de Tula',
-    description: 'Lograr mejores condiciones en materia de SALUD, en beneficio de los habitantes de la Zona Metropolitana de Tula',
+    description: 'Lograr mejores condiciones en materia de SALUD, en beneficio de los habitantes de la Zona Metropolitana de Tula.',
   },
   {
     image: 'https://via.placeholder.com/250?text=Slide+3',
     title: 'Subcomisión de Protección Civil y Gestión Integral de Riesgos de la Zona Metropolitana de Tula',
-    description: 'Lograr mejores condiciones en materia de PROTECCIÓN CIVIL Y GESTIÓN INTEGRAL DE RIESGOS, en beneficio de los habitantes de la Zona Metropolitana de Tula',
+    description: 'Lograr mejores condiciones en materia de PROTECCIÓN CIVIL Y GESTIÓN INTEGRAL DE RIESGOS, en beneficio de los habitantes de la Zona Metropolitana de Tula.',
   },
 
   {
     image: 'https://via.placeholder.com/250?text=Slide+4',
     title: 'Subcomisión de Movilidad de la Zona Metropolitana de Tula',
-    description: 'Lograr mejores condiciones en materia de MOVILIDAD, en beneficio de los habitantes de la Zona Metropolitana de Tula',
+    description: 'Lograr mejores condiciones en materia de MOVILIDAD, en beneficio de los habitantes de la Zona Metropolitana de Tula.',
   },
   {
     image: 'https://via.placeholder.com/250?text=Slide+5',
     title: 'Subcomisión de Medio Ambiente de la Zona Metropolitana de Tula',
-    description: 'Lograr mejores condiciones en materia de MEDIO AMBIENTE, en beneficio de los habitantes de la Zona Metropolitana de Tula',
+    description: 'Lograr mejores condiciones en materia de MEDIO AMBIENTE, en beneficio de los habitantes de la Zona Metropolitana de Tula.',
   },
   {
     image: 'https://via.placeholder.com/250?text=Slide+6',
     title: 'Subcomisión de Gobernanza Metropolitana de la Zona Metropolitana de Tula',
-    description: 'Lograr mejores condiciones en materia de GOBERNANZA METROPOLITANA, en beneficio de los habitantes de la Zona Metropolitana de Tula',
+    description: 'Lograr mejores condiciones en materia de GOBERNANZA METROPOLITANA, en beneficio de los habitantes de la Zona Metropolitana de Tula.',
   },
   {
     image: 'https://via.placeholder.com/250?text=Slide+7',
     title: 'Subcomisión de Desarrollo Económico de la Zona Metropolitana de Tula',
-    description: 'Lograr mejores condiciones en materia de DESARROLLO ECONÓMICO, en beneficio de los habitantes de la Zona Metropolitana de Tula',
+    description: 'Lograr mejores condiciones en materia de DESARROLLO ECONÓMICO, en beneficio de los habitantes de la Zona Metropolitana de Tula.',
   },
   {
     image: 'https://via.placeholder.com/250?text=Slide+8',
     title: 'Subcomisión de Asentamientos Humanos de la Zona Metropolitana de Tula',
-    description: 'Lograr mejores condiciones en materia de ASENTAMIENTOS HUMANOS, en beneficio de los habitantes de la Zona Metropolitana de Tula',
+    description: 'Lograr mejores condiciones en materia de ASENTAMIENTOS HUMANOS, en beneficio de los habitantes de la Zona Metropolitana de Tula.',
   },
   {
     image: 'https://via.placeholder.com/250?text=Slide+9',
     title: 'Subcomisión de Agua y Alcantarillado de la Zona Metropolitana de Tula',
-    description: 'Lograr mejores condiciones en materia de AGUA Y ALCANTARILLADO, en beneficio de los habitantes de la Zona Metropolitana de Tula',
+    description: 'Lograr mejores condiciones en materia de AGUA Y ALCANTARILLADO, en beneficio de los habitantes de la Zona Metropolitana de Tula.',
   }
 ];
 
