08c54f83e2235cb42db4c237c0d8b3edc49362e1
gomez50057 <gomez.50057@gmail.com>
2024-08-17 19:40:42 -0600

    form dropzone

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -27,18 +27,36 @@ const Formulario = () => {
   const [files, setFiles] = useState([]);
 
   const handleDrop = (acceptedFiles) => {
-    setFiles(acceptedFiles.map(file => Object.assign(file, {
-      preview: URL.createObjectURL(file)
-    })));
+    const newFiles = acceptedFiles.map(file => ({
+      file,
+      preview: URL.createObjectURL(file),
+      progress: 0, // Iniciar el progreso en 0 para cada archivo
+      completed: false // Nuevo estado para marcar como completado
+    }));
+    setFiles(prevFiles => [...prevFiles, ...newFiles]);
+
+    // Simular el progreso de carga para cada archivo
+    newFiles.forEach((newFile, index) => {
+      const interval = setInterval(() => {
+        setFiles(prevFiles => {
+          const updatedFiles = [...prevFiles];
+          const currentFile = updatedFiles[prevFiles.length - newFiles.length + index];
+
+          if (currentFile.progress >= 100) {
+            clearInterval(interval);
+            currentFile.completed = true; // Marcar como completado
+          } else {
+            currentFile.progress += 10;
+          }
+
+          return updatedFiles;
+        });
+      }, 100);
+    });
   };
 
-  const formatPhoneNumber = (value) => {
-    const cleanedValue = value.replace(/\D/g, '').slice(0, 10); // Limitar a 10 dígitos
-    const match = cleanedValue.match(/^(\d{3})(\d{4})(\d{3})$/);
-    if (match) {
-      return `${match[1]} ${match[2]} ${match[3]}`;
-    }
-    return cleanedValue; // Devuelve el valor formateado o parcialmente formateado
+  const handleRemoveFile = (fileToRemove) => {
+    setFiles(files.filter(file => file.file !== fileToRemove));
   };
 
   return (
@@ -62,7 +80,7 @@ const Formulario = () => {
         console.log('Formulario enviado:', values);
       }}
     >
-      {({ values, setFieldValue }) => (
+      {({ setFieldValue }) => (
         <Form className="formulario-container">
           <h2>Datos Generales</h2>
           <div className="form-group">
@@ -73,55 +91,45 @@ const Formulario = () => {
           <div className="form-row">
             <div className="form-group">
               <label>Nombre:</label>
-              <Field name="nombre" type="text" className="input-field" placeholder="Julio" />
+              <Field name="nombre" type="text" className="input-field" />
               <ErrorMessage name="nombre" component="div" className="error-message" />
             </div>
 
             <div className="form-group">
               <label>Apellido Paterno:</label>
-              <Field name="apellidoPaterno" type="text" className="input-field" placeholder="Menchaca"/>
+              <Field name="apellidoPaterno" type="text" className="input-field" />
               <ErrorMessage name="apellidoPaterno" component="div" className="error-message" />
             </div>
 
             <div className="form-group">
               <label>Apellido Materno:</label>
-              <Field name="apellidoMaterno" type="text" className="input-field" placeholder="Salazar"/>
+              <Field name="apellidoMaterno" type="text" className="input-field" />
               <ErrorMessage name="apellidoMaterno" component="div" className="error-message" />
             </div>
           </div>
 
           <div className="form-group">
             <label>Área de Adscripción:</label>
-            <Field name="areaAdscripcion" type="text" className="input-field" placeholder="jefazo"/>
+            <Field name="areaAdscripcion" type="text" className="input-field" />
             <ErrorMessage name="areaAdscripcion" component="div" className="error-message" />
           </div>
 
           <div className="form-row">
             <div className="form-group">
               <label>Teléfono:</label>
-              <Field 
-                name="telefono" 
-                type="text" 
-                className="input-field" 
-                placeholder="771 717 6000" 
-                value={values.telefono}  // Usar el valor formateado
-                onChange={(event) => {
-                  const formattedPhone = formatPhoneNumber(event.target.value);
-                  setFieldValue('telefono', formattedPhone);
-                }}
-              />
+              <Field name="telefono" type="text" className="input-field" />
               <ErrorMessage name="telefono" component="div" className="error-message" />
             </div>
 
             <div className="form-group">
               <label>Extensión:</label>
-              <Field name="extension" type="text" className="input-field" placeholder="6633"/>
+              <Field name="extension" type="text" className="input-field" />
               <ErrorMessage name="extension" component="div" className="error-message" />
             </div>
 
             <div className="form-group">
               <label>Correo Electrónico:</label>
-              <Field name="correo" type="email" className="input-field" placeholder="cg.planeacion@hidalgo.gob.mx"/>
+              <Field name="correo" type="email" className="input-field" />
               <ErrorMessage name="correo" component="div" className="error-message" />
             </div>
           </div>
@@ -129,7 +137,7 @@ const Formulario = () => {
           <h2>Acuerdo</h2>
           <div className="form-group">
             <label>Nombre del Acuerdo:</label>
-            <Field name="nombreAcuerdo" type="text" className="input-field" placeholder=""/>
+            <Field name="nombreAcuerdo" type="text" className="input-field" />
             <ErrorMessage name="nombreAcuerdo" component="div" className="error-message" />
           </div>
 
@@ -156,9 +164,29 @@ const Formulario = () => {
             </Dropzone>
             <ErrorMessage name="documentos" component="div" className="error-message" />
             <div className="file-preview">
-              {files.map((file, index) => (
+              {files.map((fileObj, index) => (
                 <div key={index} className="file-preview-item">
-                  <img src={file.preview} alt={`Documento ${index + 1}`} />
+                  <img src={fileObj.preview} alt={`Documento ${index + 1}`} />
+                  {fileObj.completed ? (
+                    <>
+                      <div className="checkmark-circle">
+                        <svg viewBox="0 0 52 52" className="checkmark">
+                          <circle cx="26" cy="26" r="25" fill="none" />
+                          <path d="M14 27l8 8 16-16" fill="none" />
+                        </svg>
+                      </div>
+                      <div className="file-details">
+                        <p>{fileObj.file.name}</p>
+                        <button type="button" onClick={() => handleRemoveFile(fileObj.file)}>
+                          Eliminar
+                        </button>
+                      </div>
+                    </>
+                  ) : (
+                    <div className="progress-bar">
+                      <div className="progress" style={{ width: `${fileObj.progress}%` }}></div>
+                    </div>
+                  )}
                 </div>
               ))}
             </div>
