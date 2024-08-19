12c0085f95349cb3269816cc012ba44c8442e495
gomez50057 <gomez.50057@gmail.com>
2024-08-19 11:22:23 -0600

    first commit

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
-// next.config.js
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-};
-
-module.exports = nextConfig;
+// // next.config.js
+// module.exports = {
+//   output: 'export',
+//   trailingSlash: true,
+//   assetPrefix: './',
+// };
