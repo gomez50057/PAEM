1a85bcfcedd5251e2b9dcdcee7da98e72ab5dfbc
gomez50057 <gomez.50057@gmail.com>
2024-12-17 18:10:51 -0600

    blogPosts

--- src/utils/blogData.js
+++ src/utils/blogData.js
@@ -10,30 +10,27 @@ export const blogPosts = [
     description: "Hidalgo fue sede de la Primera Sesión Ordinaria del Consejo de Desarrollo Metropolitano del Valle de México 2023 desde Pachuca de Soto, Hidalgo, la cual se llevó a cabo el día 19 de mayo de 2023. \n Se contó con la presencia de la ex Jefa de Gobierno de la Ciudad de México, Claudia Sheinbaum, el ex gobernador del Estado de México, Alfredo Del Mazo Maza y siendo el anfitrión el gobernador Julio Menchaca; así como representantes de los tres niveles de gobierno de los estados, en la cual se aprobaron **6 grandes acuerdos**: \n*	Aprobación de la Agenda Metropolitana.\n*	Crear un corredor logístico-industrial Ciudad de México, Hidalgo y Estado de México.\n*	Creación de mecanismos financieros para el desarrollo metropolitano. \n*	Análisis para la integración de nuevos municipios de Hidalgo a la ZMVM.\n*	Actualización de las condiciones de aprovechamiento de los recursos hídricos de la ZMVM.\n*	Análisis de la propuesta de reglamento interior del Consejo de Desarrollo Metropolitano del Valle de México.",
     date: "19 de mayo, 2023",
     image: "https://via.placeholder.com/400x300",
-    quote: "El lugar donde la naturaleza te deja sin palabras.",
-    category: "ZMP"
+    category: "ZMVM"
   },
   {
     name: "Segunda Sesión Ordinaria del Consejo de Desarrollo Metropolitano del Valle de México",
-    description: " **Acuerdan gobernadores consolidar el Corredor Logístico Industrial CDMX- HIDALGO-EDOMEX del AIFA.**  \n El 24 noviembre del 2023 se llevó a cabo la Segunda Sesión Ordinaria del Consejo de Desarrollo Metropolitano del Valle de México donde participaron la gobernadora del Estado de México, el gobernador de Hidalgo y el Jefe de gobierno de la CDMX para impulsar el programa de ordenación de la Zona Metropolitana del Valle de México y la continuación de los trabajos de las Comisiones Metropolitanas y la Agenda Metropolitana. Destacando el impacto del Corredor Logístico Industrial. \n Lo anterior con el objetivo de establecer las bases para crear una visión compartida a largo plazo en la ordenación del territorio, teniendo como resultado la transformación metropolitana del Valle de México.",
+    description: " **Acuerdan gobernadores consolidar el Corredor Logístico Industrial CDMX-HIDALGO-EDOMEX del AIFA.**  \n El 24 noviembre del 2023 se llevó a cabo la Segunda Sesión Ordinaria del Consejo de Desarrollo Metropolitano del Valle de México donde participaron la gobernadora del Estado de México, el gobernador de Hidalgo y el Jefe de gobierno de la CDMX para impulsar el programa de ordenación de la Zona Metropolitana del Valle de México y la continuación de los trabajos de las Comisiones Metropolitanas y la Agenda Metropolitana. Destacando el impacto del Corredor Logístico Industrial. \n Lo anterior con el objetivo de establecer las bases para crear una visión compartida a largo plazo en la ordenación del territorio, teniendo como resultado la transformación metropolitana del Valle de México.",
     date: "24 noviembre, 2023",
     image: "https://via.placeholder.com/400x300",
-    quote: "El invierno más hermoso que jamás hayas visto.",
-    category: "ZMTula"
+    category: "ZMVM"
   },
   {
     name: "Comisiones Metropolitanas de Gestión Integral de Riesgos (COMEPROC) y de Seguridad Pública y Procuración de Justicia (CMSPyPJ)",
     description: "El 09 marzo del 2023 se llevó a cabo una reunión con titulares de Hidalgo, Ciudad de México y Estado de México en temas de seguridad pública, procuración de justicia y gestión integral de riesgos y protección civil para analizar,  **evaluar y dar seguimiento a las propuestas en materia de gestión integral de riesgos y protección civil** , así como seguridad pública y procuración de justicia.",
     date: "9 de marzo, 2023",
     image: "https://via.placeholder.com/400x300",
-    category: "ZMTulancingo"
+    category: "ZMVM"
   },
   {
     name: "Red Nacional Metropolitana 2024",
     description: " **Gobernanza Metropolitana del Valle de México.** \n El pasado 29 de noviembre del 2024, el titular de Planeación y Prospectiva, Mtro. Miguel Ángel Tello Vargas, participó con una conferencia en la edición de la Red Nacional Metropolitana llevada a cabo en el estado de Tlaxcala. Donde destacó que, para garantizar una efectiva gobernanza metropolitana, es fundamental la coordinación entre los 3 órdenes de gobierno, en donde la escala metropolitana no conoce fronteras ni límites de competencia gubernamental. \n El titular destacó 4 principales pasos que se han llevado a cabo en la Zona Metropolitana del Valle de México para garantizar una efectiva coordinación: \n* Establecimiento de una agenda de trabajo permanente \n* Colaboración con identidades federativas \n* Trabajo cercano y permanente con autoridades municipales \n* Construcción con los sectores sociales",
     date: "29 de noviembre, 2029",
     image: "https://via.placeholder.com/400x300",
-    quote: "Donde los tulipanes cuentan historias.",
     category: "ZMVM"
   },
   {
@@ -41,37 +38,38 @@ export const blogPosts = [
     description: "Como parte de los trabajos para la actualización del Programa de Ordenamiento de la Zona Metropolitana del Valle de México. El titular de la #UPLAPH, Mtro. Miguel Ángel Tello Vargas se reunió el pasado 13 de noviembre, con representantes del Gobierno de la Ciudad de México, Estado de México y la SEDATU, donde se abordaron los **principales retos y acciones en materia de vivienda** de las entidades que conforman la #ZMVM. \n https://www.facebook.com/share/p/19Yah23Hf3/ ",
     date: "13 de noviembre, 2024",
     image: "https://via.placeholder.com/400x300",
-    category: "ZMP"
+    category: "ZMVM"
   },
   {
-    name: "Sed viverra purus",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed viverra, purus a viverra tincidunt, augue odio vestibulum orci, ac fringilla dui turpis eget erat.",
-    date: "19 de septiembre, 2024",
+    name: "6ta reunión previas actualización del Programa de Ordenamiento de la Zona Metropolitana del Valle de México",
+    description: " **_sede Hidalgo_** \n Como parte de la Agenda Metropolitana del Valle de México, llevamos a cabo una reunión el 30 de octubre de 2024, donde se discutieron temas sobre asentamientos humanos, equipamiento e infraestructura, como parte de los **trabajos previos a la actualización del Programa de Ordenación de la ZMVM**, entre las entidades que conforman la #ZMVM: Estado de México, Ciudad de México e Hidalgo, junto con la SEDATU. ",
+    date: "30 de octubre, 2024",
     image: "https://via.placeholder.com/400x300",
-    category: "ZMP"
+    category: "ZMVM"
   },
   {
-    name: "Sed viverra purus",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed viverra, purus a viverra tincidunt, augue odio vestibulum orci, ac fringilla dui turpis eget erat.",
-    date: "19 de septiembre, 2024",
+    name: "3ra reunión previas actualización del Programa de Ordenamiento de la Zona Metropolitana del Valle de México",
+    description: "En seguimiento a los trabajos del programa de ordenamiento de la Zona Metropolitana del Valle de México, el titular de la #UPLAPH, Mtro. Miguel Ángel Tello Vargas se reunió el 12 de septiembre de 2024, con representantes del Gobierno del Estado de México, Gobierno de la Ciudad de México, SEDATU y Lyzbeth Robles, titular de la Secretaría de Movilidad y Transporte de Hidalgo. \n En dicha reunión se hizo hincapié en que el **transporte público en la #ZMVM** debe ser seguro, sustentable e interconectado, para que las personas se desplacen con facilidad, sin importar las divisiones municipales o estatales. \n En reunión con representantes del Gobierno del Estado de México, Gobierno de la Ciudad de México, SEDATU, con Lyzbeth Robles, titular de la Secretaría de Movilidad y Transporte de Hidalgo, dimos seguimiento a los avances del programa de ordenamiento de la Zona Metropolitana del Valle de México.",
+    date: "12 de septiembre, 2024",
     image: "https://via.placeholder.com/400x300",
-    category: "ZMP"
+    category: "ZMVM"
   },
   {
-    name: "Sed viverra purus",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed viverra, purus a viverra tincidunt, augue odio vestibulum orci, ac fringilla dui turpis eget erat.",
-    date: "19 de septiembre, 2024",
+    name: "2da reunión previas actualización del Programa de Ordenamiento de la Zona Metropolitana del Valle de México",
+    description: "Con representantes del Gobierno del Estado de México y Gobierno de la Ciudad de México, el titular de la #UPLAPH, Miguel Tello participó en la reunión de seguimiento con el equipo de transición de la SEDATU, llevada a cabo el 29 de agosto de 2024, donde se abordaron temas relacionados con la movilidad y el crecimiento ordenado de la Zona Metropolitana del Valle de México. \n **En esta reunión, se promovieron acciones que mejoran la calidad de vida de las y los hidalguenses!**",
+    date: "29 de agosto , 2024",
     image: "https://via.placeholder.com/400x300",
-    category: "ZMP"
+    category: "ZMVM"
   },
 
 
 
   // {
   //   name: "Sed viverra purus",
-  //   description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed viverra, purus a viverra tincidunt, augue odio vestibulum orci, ac fringilla dui turpis eget erat.",
+  //   description: "texto",
   //   date: "19 de septiembre, 2024",
   //   image: "https://via.placeholder.com/400x300",
+  //   quote: "El lugar donde la naturaleza te deja sin palabras.",
   //   category: "ZMP"
   // },
 ];
