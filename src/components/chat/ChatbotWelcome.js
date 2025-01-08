db0d149a478e4306eeb73d35f55a4c3785a235ae
gomez50057 <gomez.50057@gmail.com>
2025-01-08 13:58:09 -0600

    Chatbot isCollapsed

--- src/components/chat/ChatbotWelcome.js
+++ src/components/chat/ChatbotWelcome.js
@@ -1,26 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './ChatbotWelcome.module.css';
 
 const ChatbotWelcome = ({ onStartChat }) => {
+  const [isCollapsed, setIsCollapsed] = useState(false);
+
+  const handleToggleCollapse = () => {
+    setIsCollapsed(!isCollapsed);
+  };
+
   return (
-    <div className={styles.chatbotWelcome}>
-      <div className={styles.header}>
-        <button className={styles.backButton}>
-          <span className={styles.arrowLeft}>&larr;</span>
-        </button>
-      </div>
-      <div className={styles.content}>
-        <img src="img/headertxt.png" alt="Metrópoli Hidalgo" className={styles.logotxt} />
-        <img src="img/sidebarRecurso.png" alt="Chatbot Logo" className={styles.logo} />
-        <h1 className={styles.title}>¡Hola!, Bienvenid@ al</h1>
-        <h2 className={styles.subtitle}>Chatbot de <span>Metrópoli Hidalgo</span></h2>
-        <p className={styles.description}>Un asistente Metropolitano</p>
-      </div>
-      <button className={styles.startButton} onClick={onStartChat}>
-        Iniciar Chat
-        <span className={styles.arrowRight}>&rarr;</span>
-      </button>
-    </div>
+    <>
+      {!isCollapsed ? (
+        <div className={styles.chatbotWelcome}>
+          <div className={styles.header}>
+            <button className={styles.backButton} onClick={handleToggleCollapse}>
+              <span className={styles.xClose}>x</span>
+            </button>
+          </div>
+          <div className={styles.content}>
+            <img src="img/headertxt.png" alt="Metrópoli Hidalgo" className={styles.logotxt} />
+            <img src="img/sidebarRecurso.png" alt="Chatbot Logo" className={styles.logo} />
+            <h1 className={styles.title}>¡Hola!, Bienvenid@ al</h1>
+            <h2 className={styles.subtitle}>Chatbot de <span>Metrópoli Hidalgo</span></h2>
+            <p className={styles.description}>Un asistente Metropolitano</p>
+          </div>
+          <button className={styles.startButton} onClick={onStartChat}>
+            Iniciar Chat
+            <span className={styles.arrowRight}>&rarr;</span>
+          </button>
+        </div>
+      ) : (
+        <img
+          src="img/sidebarRecurso.png"
+          alt="Chatbot Logo"
+          className={styles.logoCollapsed}
+          onClick={handleToggleCollapse}
+        />
+      )}
+    </>
   );
 };
 
