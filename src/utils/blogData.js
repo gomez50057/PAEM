9792a8040f71028e2c21fc8a3c7e35a12c924fed
gomez50057 <gomez.50057@gmail.com>
2025-01-10 13:22:15 -0600

    blog orden

--- src/utils/blogData.js
+++ src/utils/blogData.js
@@ -8,29 +8,6 @@ import styles from "../components/blog/FullPost.module.css";
 
 export const blogPosts = [
   {
-    name: "Primera Sesión Ordinaria del Consejo de Desarrollo Metropolitano del Valle de México",
-    description: "Hidalgo fue sede de la Primera Sesión Ordinaria del Consejo de Desarrollo Metropolitano del Valle de México 2023 desde Pachuca de Soto, Hidalgo, la cual se llevó a cabo el día 19 de mayo de 2023. \n Se contó con la presencia de la ex Jefa de Gobierno de la Ciudad de México, Claudia Sheinbaum, el ex gobernador del Estado de México, Alfredo Del Mazo Maza y siendo el anfitrión el gobernador Julio Menchaca; así como representantes de los tres niveles de gobierno de los estados, en la cual se aprobaron **6 grandes acuerdos**: \n*	Aprobación de la Agenda Metropolitana.\n*	Crear un corredor logístico-industrial Ciudad de México, Hidalgo y Estado de México.\n*	Creación de mecanismos financieros para el desarrollo metropolitano. \n*	Análisis para la integración de nuevos municipios de Hidalgo a la ZMVM.\n*	Actualización de las condiciones de aprovechamiento de los recursos hídricos de la ZMVM.\n*	Análisis de la propuesta de reglamento interior del Consejo de Desarrollo Metropolitano del Valle de México.",
-    date: "19 de mayo, 2023",
-       quote: "El lugar donde la naturaleza te deja sin palabras.",
-
-    image: "/img/noticias/ZMVM/Primera Sesión Ordinaria del Consejo de Desarrollo Metropolitano del Valle de México.jpg",
-    category: "ZMVM"
-  },
-  {
-    name: "Segunda Sesión Ordinaria del Consejo de Desarrollo Metropolitano del Valle de México",
-    description: " **Acuerdan gobernadores consolidar el Corredor Logístico Industrial CDMX-HIDALGO-EDOMEX del AIFA.**  \n El 24 noviembre del 2023 se llevó a cabo la Segunda Sesión Ordinaria del Consejo de Desarrollo Metropolitano del Valle de México donde participaron la gobernadora del Estado de México, el gobernador de Hidalgo y el Jefe de gobierno de la CDMX para impulsar el programa de ordenación de la Zona Metropolitana del Valle de México y la continuación de los trabajos de las Comisiones Metropolitanas y la Agenda Metropolitana. Destacando el impacto del Corredor Logístico Industrial. \n Lo anterior con el objetivo de establecer las bases para crear una visión compartida a largo plazo en la ordenación del territorio, teniendo como resultado la transformación metropolitana del Valle de México.",
-    date: "24 noviembre, 2023",
-    image: "/img/noticias/ZMVM/Segunda Sesión Ordinaria del Consejo de Desarrollo Metropolitano del Valle de México.jpg",
-    category: "ZMVM"
-  },
-  {
-    name: "Comisiones Metropolitanas de Gestión Integral de Riesgos (COMEPROC) y de Seguridad Pública y Procuración de Justicia (CMSPyPJ)",
-    description: "El 09 marzo del 2023 se llevó a cabo una reunión con titulares de Hidalgo, Ciudad de México y Estado de México en temas de seguridad pública, procuración de justicia y gestión integral de riesgos y protección civil para analizar,  **evaluar y dar seguimiento a las propuestas en materia de gestión integral de riesgos y protección civil** , así como seguridad pública y procuración de justicia.",
-    date: "9 de marzo, 2023",
-    image: "/img/noticias/ZMVM/Comisiones Metropolitanas de Gestión Integral de Riesgos (COMEPROC) y de Seguridad Pública y Procuración de Justicia (CMSPyPJ).jpg",
-    category: "ZMVM"
-  },
-  {
     name: "Red Nacional Metropolitana 2024",
     description: " **Gobernanza Metropolitana del Valle de México.** \n El pasado 29 de noviembre del 2024, el titular de Planeación y Prospectiva, Mtro. Miguel Ángel Tello Vargas, participó con una conferencia en la edición de la Red Nacional Metropolitana llevada a cabo en el estado de Tlaxcala. Donde destacó que, para garantizar una efectiva gobernanza metropolitana, es fundamental la coordinación entre los 3 órdenes de gobierno, en donde la escala metropolitana no conoce fronteras ni límites de competencia gubernamental. \n El titular destacó 4 principales pasos que se han llevado a cabo en la Zona Metropolitana del Valle de México para garantizar una efectiva coordinación: \n* Establecimiento de una agenda de trabajo permanente \n* Colaboración con identidades federativas \n* Trabajo cercano y permanente con autoridades municipales \n* Construcción con los sectores sociales",
     date: "29 de noviembre, 2024",
@@ -65,6 +42,31 @@ export const blogPosts = [
     image: "/img/noticias/ZMVM/2da reunión previas actualización del Programa de Ordenamiento de la Zona Metropolitana del Valle de México.jpg",
     category: "ZMVM"
   },
+  {
+    name: "Segunda Sesión Ordinaria del Consejo de Desarrollo Metropolitano del Valle de México",
+    description: " **Acuerdan gobernadores consolidar el Corredor Logístico Industrial CDMX-HIDALGO-EDOMEX del AIFA.**  \n El 24 noviembre del 2023 se llevó a cabo la Segunda Sesión Ordinaria del Consejo de Desarrollo Metropolitano del Valle de México donde participaron la gobernadora del Estado de México, el gobernador de Hidalgo y el Jefe de gobierno de la CDMX para impulsar el programa de ordenación de la Zona Metropolitana del Valle de México y la continuación de los trabajos de las Comisiones Metropolitanas y la Agenda Metropolitana. Destacando el impacto del Corredor Logístico Industrial. \n Lo anterior con el objetivo de establecer las bases para crear una visión compartida a largo plazo en la ordenación del territorio, teniendo como resultado la transformación metropolitana del Valle de México.",
+    date: "24 noviembre, 2023",
+    image: "/img/noticias/ZMVM/Segunda Sesión Ordinaria del Consejo de Desarrollo Metropolitano del Valle de México.jpg",
+    category: "ZMVM"
+  },
+  {
+    name: "Primera Sesión Ordinaria del Consejo de Desarrollo Metropolitano del Valle de México",
+    description: "Hidalgo fue sede de la Primera Sesión Ordinaria del Consejo de Desarrollo Metropolitano del Valle de México 2023 desde Pachuca de Soto, Hidalgo, la cual se llevó a cabo el día 19 de mayo de 2023. \n Se contó con la presencia de la ex Jefa de Gobierno de la Ciudad de México, Claudia Sheinbaum, el ex gobernador del Estado de México, Alfredo Del Mazo Maza y siendo el anfitrión el gobernador Julio Menchaca; así como representantes de los tres niveles de gobierno de los estados, en la cual se aprobaron **6 grandes acuerdos**: \n*	Aprobación de la Agenda Metropolitana.\n*	Crear un corredor logístico-industrial Ciudad de México, Hidalgo y Estado de México.\n*	Creación de mecanismos financieros para el desarrollo metropolitano. \n*	Análisis para la integración de nuevos municipios de Hidalgo a la ZMVM.\n*	Actualización de las condiciones de aprovechamiento de los recursos hídricos de la ZMVM.\n*	Análisis de la propuesta de reglamento interior del Consejo de Desarrollo Metropolitano del Valle de México.",
+    date: "19 de mayo, 2023",
+       quote: "El lugar donde la naturaleza te deja sin palabras.",
+
+    image: "/img/noticias/ZMVM/Primera Sesión Ordinaria del Consejo de Desarrollo Metropolitano del Valle de México.jpg",
+    category: "ZMVM"
+  },
+  {
+    name: "Comisiones Metropolitanas de Gestión Integral de Riesgos (COMEPROC) y de Seguridad Pública y Procuración de Justicia (CMSPyPJ)",
+    description: "El 09 marzo del 2023 se llevó a cabo una reunión con titulares de Hidalgo, Ciudad de México y Estado de México en temas de seguridad pública, procuración de justicia y gestión integral de riesgos y protección civil para analizar,  **evaluar y dar seguimiento a las propuestas en materia de gestión integral de riesgos y protección civil** , así como seguridad pública y procuración de justicia.",
+    date: "9 de marzo, 2023",
+    image: "/img/noticias/ZMVM/Comisiones Metropolitanas de Gestión Integral de Riesgos (COMEPROC) y de Seguridad Pública y Procuración de Justicia (CMSPyPJ).jpg",
+    category: "ZMVM"
+  },
+
+
 
 
 
