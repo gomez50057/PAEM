3b69fb2898de3fcf6d284ea80967267e745ca2b4
gomez50057 <gomez.50057@gmail.com>
2025-04-16 17:17:36 -0600

    noticia de 14va reunión de coordinación para la elaboración del Programa de Ordenamiento Metropolitano del Valle de México

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,12 @@
 import Footer from '../components/shared/Footer';
 import '../styles/globals.css';
 
+export const metadata = {
+  title: "Metrópoli Hidalgo ",
+  description: "Las zonas metropolitanas son áreas donde varios municipios convergen conformando un continuo urbano",
+};
+
+
 export default function RootLayout({ children }) {
   return (
     <html lang="es">
